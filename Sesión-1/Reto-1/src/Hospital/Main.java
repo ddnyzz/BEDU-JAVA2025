47e618ae95e5483390847bae789fc02598999b7e
Daniela Castillo <152355250+ddnyzz@users.noreply.github.com>
2025-05-19 10:28:02 -0600

    Update Main.java

--- Sesión-1/Reto-1/src/Hospital/Main.java
+++ Sesión-1/Reto-1/src/Hospital/Main.java
@@ -1,1 +1,22 @@
+package Hospital;
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Paciente paciente1 = new Paciente();
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Nombre del paciente: ");
+        paciente1.Nombre = input.nextLine();
+
+        System.out.print("Edad del paciente: ");
+        paciente1.Edad = input.nextInt();
+        input.nextLine();
+
+        System.out.print("Expediente: ");
+        paciente1.NumExp = input.nextLine();
+      
+        paciente1.mostrarInformacion();
+    }
+}
