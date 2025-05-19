304fb3f7dd0dcb955bb5668a47c6581a460ce91c
Daniela Castillo <152355250+ddnyzz@users.noreply.github.com>
2025-05-19 10:50:24 -0600

    Update Main.java

--- Sesión-1/Reto-2/Main.java
+++ Sesión-1/Reto-2/Main.java
@@ -1,1 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Entrada entrada1 = new Entrada("Concierto", 450.0);
+        Entrada entrada2 = new Entrada("Obra de teatro", 350.0);
 
+        entrada1.MostrarInformacion();
+        entrada2.MostrarInformacion();
+    }
+}
