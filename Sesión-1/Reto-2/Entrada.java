e591ee73500ba7ea27132ac06c021058bcc816b1
Daniela Castillo <152355250+ddnyzz@users.noreply.github.com>
2025-05-19 10:48:02 -0600

    Update Entrada.java

--- Sesión-1/Reto-2/Entrada.java
+++ Sesión-1/Reto-2/Entrada.java
@@ -1,1 +1,14 @@
+public class Entrada {
+    
+    String NombreEvento;
+    double PrecioEntrada;
 
+    public Entrada(String nombre, double precio) {
+        this.NombreEvento = nombre;
+        this.PrecioEntrada = precio;
+    }
+
+     public void MostrarInformacion(){
+        System.out.println("Evento: " + NombreEvento + " | Precio: $" + PrecioEntrada);
+    }
+}
