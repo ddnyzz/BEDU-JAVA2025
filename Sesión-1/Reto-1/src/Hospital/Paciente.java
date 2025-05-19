4af9b4a13803360438a1de35f9c292bced0221a0
Daniela Castillo <152355250+ddnyzz@users.noreply.github.com>
2025-05-19 10:32:10 -0600

    Update Paciente.java

--- Sesión-1/Reto-1/src/Hospital/Paciente.java
+++ Sesión-1/Reto-1/src/Hospital/Paciente.java
@@ -1,1 +1,15 @@
+package Hospital;
 
+public class Paciente {
+    String Nombre, NumExp;
+    int Edad;
+  
+    public void mostrarInformacion(){
+      
+        System.out.println("\nInformación del paciente:");
+        System.out.println("Paciente: " + Nombre);
+        System.out.println("Edad: " + Edad);
+        System.out.println("Expediente: " + NumExp);
+    }
+
+}
