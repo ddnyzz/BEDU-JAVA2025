989fc876c4b1358da01bb06debf4e6f1c8c84a62
Daniela Castillo <152355250+ddnyzz@users.noreply.github.com>
2025-05-19 20:18:42 -0600

    Update SimuladorFarmacia.java

--- Sesión 2/Reto 1/SimuladorFarmacia.java
+++ Sesión 2/Reto 1/SimuladorFarmacia.java
@@ -1,1 +1,21 @@
+public class SimuladorFarmacia {
+    String Nombre;
+    double Unidad, Total, Pagar, Descuento;
+    int Cantidad;
+    boolean DescuentoT;
 
+    public void Medicamento(){
+        Total = Unidad * Cantidad;
+        DescuentoT = Total > 500;
+        Descuento = DescuentoT ? Total * 0.15 : 0;
+        Pagar = Total - Descuento;
+
+        System.out.println("Medicamento: " + Nombre);
+        System.out.println("Cantidad: " + Cantidad);
+        System.out.println("Precio unitario: $" + Unidad);
+        System.out.println("Total sin descuento: $" + Total);
+        System.out.println("¿Aplica descuento?: " + DescuentoT);
+        System.out.println("Descuento: $" + Descuento);
+        System.out.println("Total a pagar: $" + Pagar);
+    }
+}
