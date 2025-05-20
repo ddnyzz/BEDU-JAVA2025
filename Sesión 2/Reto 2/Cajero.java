9e81a2abde18b7d69386f66b237a0c49f1b9953c
Daniela Castillo <152355250+ddnyzz@users.noreply.github.com>
2025-05-19 20:35:03 -0600

    Update Cajero.java

--- Sesión 2/Reto 2/Cajero.java
+++ Sesión 2/Reto 2/Cajero.java
@@ -1,1 +1,23 @@
+public class CajeroAutomatico{
 
+    public void Consulta(double SaldoInicial){
+        System.out.println("Tu saldo es: " + SaldoInicial);
+    }
+
+    public double Deposito(double Saldo, double SaldoADepositar){
+        return saldo + SaldoADepositar;
+    }
+
+    public double retiro(double Saldo, double Retiro){
+        double Total;
+        boolean Suficiente = Saldo >= Retiro;
+        Total = Suficiente ? Saldo - Retiro: Saldo;
+        if (Suficiente){
+            System.out.println("Su saldo actualizado es: " + Total);
+        } else {
+            System.out.println("No cuenta con saldo suficiente.");
+            System.out.println("Su saldo actual es de: " + Total);
+        }
+        return Total;
+    }
+}
