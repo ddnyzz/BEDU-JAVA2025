cf3aea95085f3853c2f0deb86a64c6a6fe85503d
Daniela Castillo <152355250+ddnyzz@users.noreply.github.com>
2025-05-20 12:20:27 -0600

    Update Main.java

--- Sesión 4/Reto 2/Main.java
+++ Sesión 4/Reto 2/Main.java
@@ -1,1 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+      
+        DeclaracionImpuestos declaracion = new DeclaracionImpuestos("XAXX010101000", 8700.0);
+        CuentaFiscal cuenta = new CuentaFiscal("XAXX010101000", 9500.0);
 
+        System.out.println("Declaración enviada por RFC: " + Declaracion.rfcContribuyente() +
+                " por $" + Declaracion.MontoDeclarado());
+
+        System.out.println("Cuenta fiscal registrada con RFC: " + cuenta.getRfc() +
+                ", saldo disponible: $" + cuenta.getSaldoDisponible());
+
+        boolean rfcValido = cuenta.validarRFC(declaracion);
+        System.out.println("RFC válido para esta cuenta?: " + rfcValido);
+    }
+}
