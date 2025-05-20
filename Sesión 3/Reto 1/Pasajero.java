b2b3a9732d51bf35c6231b434c27b9b0ceb339ff
Daniela Castillo <152355250+ddnyzz@users.noreply.github.com>
2025-05-19 21:51:07 -0600

    Update Pasajero.java

--- Sesión 3/Reto 1/Pasajero.java
+++ Sesión 3/Reto 1/Pasajero.java
@@ -1,1 +1,18 @@
+public class Pasajero {
+    private String Nombre;
+    private String Pasaporte;
+
+    public Pasajero(String Nombre, String Pasaporte) {
+        this.Nombre = Nombre;
+        this.Pasaporte = Pasaporte;
+    }
+
+     public String getNombre() {
+        return Nombre;
+    }
+
+    public String getPasaporte() {
+        return Pasaporte;
+    }
+}
 
