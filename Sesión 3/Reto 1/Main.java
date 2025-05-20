d847848e5efb2fba464ae6c7edb3dd6eed4a020f
Daniela Castillo <152355250+ddnyzz@users.noreply.github.com>
2025-05-19 21:38:18 -0600

    Update Main.java

--- Sesión 3/Reto 1/Main.java
+++ Sesión 3/Reto 1/Main.java
@@ -1,1 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+      
+        Vuelo vuelo1 = new Vuelo("UX123", "París", "14:30");
+        Pasajero pasajero1 = new Pasajero("Ana Martínez", "X123456");
 
+        //reserva1
+        if (vuelo1.reservarAsiento(pasajero1)) {
+            System.out.println("Reserva exitosa");
+        } else {
+            System.out.println("Asiento ocupado");
+        }
+
+        System.out.println(vuelo1.obtenerItinerario());
+
+        //cancelar
+        System.out.println("Cancelando");
+        vuelo1.cancelarReserva();
+        System.out.println(vuelo1.obtenerItinerario());
+
+        if (vuelo1.reservarAsiento("Mario Gonzalez", "Z987654")) {
+            System.out.println("Reserva exitosa");
+        } else {
+            System.out.println("Asiento ocupado");
+        }
+        System.out.println(vuelo1.obtenerItinerario());
+    }
+}
