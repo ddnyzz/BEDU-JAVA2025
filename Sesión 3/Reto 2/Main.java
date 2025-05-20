ae0c5b9c2bae8396737ab8dbe13f10c5788c7cfc
Daniela Castillo <152355250+ddnyzz@users.noreply.github.com>
2025-05-19 22:27:26 -0600

    Update Main.java

--- Sesión 3/Reto 2/Main.java
+++ Sesión 3/Reto 2/Main.java
@@ -1,1 +1,12 @@
+package Sesion_3.Reto_2;
 
+public class Principal {
+    public static void main(String[] args) {
+      
+        Factura factura1 = new Factura(2500.0, "Servicio de consultoría", "ABCC010101XYZ");
+        Factura factura2 = new Factura(1800.0, "Reparación de equipo", null);
+
+        System.out.println(factura1.getResumen());
+        System.out.println(factura2.getResumen());
+    }
+}
