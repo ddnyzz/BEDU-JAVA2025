e7e918587874897710903b16493d31e3d302e901
Daniela Castillo <152355250+ddnyzz@users.noreply.github.com>
2025-05-19 22:40:33 -0600

    Update Factura.java

--- Sesión 3/Reto 2/Factura.java
+++ Sesión 3/Reto 2/Factura.java
@@ -1,1 +1,25 @@
+package Sesion_3.Reto_2;
 
+import java.util.Optional;
+
+public class Factura {
+  
+    private double Monto; 
+    private String Descripcion; 
+    private Optional<String> rfc; 
+
+    public Factura(double Monto, String Mescripcion, String rfc) {
+        this.Monto = Monto;
+        this.Descripcion = Descripcion;
+        this.rfc = Optional.ofNullable(rfc);
+    }
+
+     public String getResumen() {
+        String estadoRFC = rfc.orElse("[No dado]");
+
+        return "Factura generada" +
+               "Descripción: " + Descripcion +
+               "Monto: $" + Monto +
+               "RFC: " + EstadoRFC;
+    }
+}
